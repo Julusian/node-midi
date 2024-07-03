c74717dcac8a6d0e7c40f53b38f6adfe70b11804
Steven Goodwin <marquisdegeek@gmail.com>
2024-07-03 14:45:24 +0100

    Add JSdoc comments to constants

--- midi.js
+++ midi.js
@@ -9,9 +9,13 @@ const { EventEmitter } = require('events');
 
 // Bring in the set of constants to reflect MIDI messages and their
 // parameters, to eliminate the need for magic numbers.
+/** An instrument list, only valid in the General MIDI standard */
 const Instruments = require('./instruments');
+/** A drum map, only valid in the General MIDI standard */
 const Drums = require('./drums');
+/** Note descriptions, with Middle C = C5 = MIDI note 60 */
 const Notes = require('./notes');
+/** Message names, including CCs */
 const Messages = require('./messages');
 
 class Input extends EventEmitter {
